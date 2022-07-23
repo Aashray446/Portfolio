11a0e925975f8690bf8822f72084aa8edf4e93b8
Aashray Katiyar <aashray446@gmail.com>
2022-07-23 12:34:46 +0530

    <Upgrade> : [Angular update to 14]

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EducationComponent } from './education/education.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+
+const routes: Routes = [
+  { component: HomeComponent, path: 'home'},
+  {component: EducationComponent, path: 'About'},
+  {component: ProjectsComponent, path: 'Projects'},
+  {component: TestimonialsComponent, path: 'Clients'},
+  {component: HomeComponent, path: '**'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    EducationComponent,
+    TestimonialsComponent,
+    ContactsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
